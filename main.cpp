ae6f2548ae1111ae090060e740132036d277cf8f
prvdn <63466607+prvdn@users.noreply.github.com>
2020-04-21 20:30:42 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
+#include <vector>
+#include <cmath>
+
+using namespace std;
+
+float sum(float a, float b, float c, float d){
+  //тут вычисления (сумма 3 чисел произвольной степени)
+  float e = 0;
+  e = pow(a+b+c,d);
+  return e;
+}
 
 int main() {
-  std::cout << "Hello World!\n";
-  
+  float a,b,c,d;
+  cin >> a >> b >> c >> d; // вводим три числа, и потом степень
+  cout <<sum(a,b,c,d); 
 }
+
