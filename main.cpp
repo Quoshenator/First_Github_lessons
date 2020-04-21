de660f09c7836b1d51043f0189e90bc40dfff9e7
Georgy666444 <geo3545@yandex.ru>
2020-04-21 18:16:46 +0000

    программа с кубами, сложением и умножением

--- main.cpp
+++ main.cpp
@@ -1,6 +1,33 @@
 #include <iostream>
-//пробный текст
-int main() {
-  std::cout << "Hello World!\n";
-  
+#include <string>
+#include <math.h>
+
+using namespace std;
+
+float x,y,z,t,s;
+int operations()
+{
+if (t==2)
+{
+s=((x*x*x)+(y*y*y)+(z*z*z));
+}
+else 
+{
+s=((x*x*x)*(y*y*y)*(z*z*z));
+}
+ cout<<"Результат:"<<endl;
+cout<<s<<endl;
+}
+
+int main()
+{    
+  cout<<"Что вы хотите сделать? Если сложение - наберите цифру 2, если умножение - любую другую"<<endl;
+  cin>>t;
+   cout<<"Введите 3 числа:"<<endl;
+   cin>>x;
+    cin>>y;
+     cin>>z;
+
+operations();
+    return 0;
 }
