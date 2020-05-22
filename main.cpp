d903af00a68cc3a810f344509d27291c3fa85d67
Larry0n <sergey_loki@mail.ru>
2020-05-22 19:36:03 +0300

    Update main.cpp
    Добавление заглушки для команды 2

--- main.cpp
+++ main.cpp
@@ -2,6 +2,10 @@
 #include <math.h>
 using namespace std;
 
+float sum(float a, float b, float c, float d){
+  //тут вычисления (сумма 3 чисел произвольной степени)
+}
+
 float sumDegrees(int n)
 {
 
