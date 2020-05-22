aa6da21dca1795be314601b2eca55cdee77bde9f
Larry0n <sergey_loki@mail.ru>
2020-05-22 22:29:29 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,25 @@
 #include <iostream>
+#include <math.h>
+using namespace std;
+
+float sumDegrees(int n)
+{
+  float d = 0;
+  int count = 1;
+  float a;
+  while(n>0)
+  {
+      cout<<"Введите "<<count<<"-е число\n";
+      cin>>a;
+      d+=pow(a,3);
+      n--;
+      count++;
+  }
+  return d;
+}
+
 
 int main() {
-  std::cout << "Hello World!\n";
-  
+
 }
+© 2020 GitHub, Inc.
