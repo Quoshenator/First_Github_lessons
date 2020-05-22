fd9eaeaa9c13cdac91a4426dababad49d0d2b979
Larry0n <sergey_loki@mail.ru>
2020-05-22 22:36:55 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,16 +1,22 @@
 #include <iostream>
 #include <math.h>
 using namespace std;
+loat x,y,z,s,t;
 float sumDegrees(int n)
 {
 //Команда 1
 }
+
 float sum(float a, float b, float c, float d)
 {
 //Команда 2
 }
 
 
+int operations()
+{
+
+}
 
 int main() {
   
