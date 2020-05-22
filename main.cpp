a83ded268dd05bba55ab7edb532422831a504524
Larry0n <sergey_loki@mail.ru>
2020-05-22 19:38:16 +0300

    Update main.cpp
    Добавление заглушки для команды 3

--- main.cpp
+++ main.cpp
@@ -2,6 +2,11 @@
 #include <math.h>
 using namespace std;
 
+float x,y,z,t,s;
+int operations()
+{
+}
+
 float sum(float a, float b, float c, float d){
   //тут вычисления (сумма 3 чисел произвольной степени)
 }
