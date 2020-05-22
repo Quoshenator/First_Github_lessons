e5ceea4f612d5b8a9da1b2131925f59300cead21
Larry0n <sergey_loki@mail.ru>
2020-05-22 19:47:17 +0300

    Update main.cpp
    Добавление общей функции

--- main.cpp
+++ main.cpp
@@ -1,19 +1,25 @@
 #include <iostream>
 #include <math.h>
 using namespace std;
-
 float x,y,z,t,s;
-int operations()
-{
-}
 
+
+
+float sumDegrees(int n)
+{
+} 
 float sum(float a, float b, float c, float d){
   //тут вычисления (сумма 3 чисел произвольной степени)
 }
 
-float sumDegrees(int n)
+int operations()
+{
+}
+
+float useFunctions()//Общая функция
 {
 
+}
 
 
 int main() {
